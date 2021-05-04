35c81201a79589ea335113c22a54c41ccf913d05
Chrigl <chriglburri@gmail.com>
2021-05-04 18:40:54 +0200

    create components

--- src/App.js
+++ src/App.js
@@ -1,90 +1,41 @@
-
-import { Button, Container, Form, Grid, Header, Icon, Segment, Statistic } from 'semantic-ui-react';
-import './App.css';
+import { Container } from "semantic-ui-react";
+import "./App.css";
+import DisplayBalance from "./components/DisplayBalance";
+import DisplayBalances from "./components/DisplayBalances";
+import EntryLine from "./components/EntryLine";
+import MainHeader from "./components/MainHeader";
+import NewEntryForm from "./components/NewEntryForm";
 
 function App() {
-  return (
-    <Container>
+    return (
+        <Container>
+            <MainHeader title="Budget" />
 
-      <Header as='h1'>Budget</Header>
+            <DisplayBalance
+                title="Your Balance:"
+                value="2550.53"
+                color="red"
+                size="small"
+            />
 
-      <Statistic size='small'>
-        <Statistic.Label>Your Balance:</Statistic.Label>
-        <Statistic.Value>2550.53</Statistic.Value> 
-      </Statistic>
+            <DisplayBalances />
 
-      <Segment textAlign='center'>
-        <Grid columns={2} defided>
-          <Grid.Row>
-            <Grid.Column>
-              <Statistic size='tiny' color='green'>
-                <Statistic.Label style={{texAlign:"left"}}>
-                  Income:
-                </Statistic.Label>
-                <Statistic.Value>2525</Statistic.Value>
-              </Statistic>
-            </Grid.Column>
-            <Grid.Column>
-              <Statistic size='tiny' color='red'>
-                <Statistic.Label style={{texAlign:"left"}}>
-                  Expenses:
-                </Statistic.Label>
-                <Statistic.Value>987987</Statistic.Value>
-              </Statistic>
-              </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+            <MainHeader title="History" type="h3" />
 
+            <EntryLine 
+                description="expense" 
+                value="321.12" 
+                isExpense={true} />
+            <EntryLine 
+            description="income" 
+            value="1234.12" />
 
-      <Header as='h3'>History</Header>
-      <Segment color='red'>
-        <Grid columns={3} textAlign='right'>
-          <Grid.Row>
-            <Grid.Column width={10} textAlign='left'>something</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>321</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' borded></Icon>
-              <Icon name='trash' borded></Icon>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-      <Segment color='green'>
-        <Grid columns={3} textAlign='right'>
-          <Grid.Row>
-            <Grid.Column width={10} textAlign='left'>something else</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>4567</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' bordered></Icon>
-              <Icon name='trash' bordered></Icon>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-
-      <Header as='h3'>Add new transaction</Header>
-      <Form unstackable>
-        <Form.Group>
-          <Form.Input 
-          icon='tags' width={12} label='Description'
-          placeholder='New thing'></Form.Input>
-          <Form.Input
-          icon='dollar'
-          iconPosition='left'
-          width = {4} label='Value'
-          placeholder="100.00"
-          ></Form.Input>
-        </Form.Group>
-        <Button.Group style={{marginTop:20}}>
-          <Button>Cancel</Button>
-          <Button.Or />
-          <Button primary>OK</Button>
-        </Button.Group>
-      </Form>
-
-    </Container>
-  );
+            <MainHeader 
+            title="Add new transaction" 
+            type="h3" />
+            <NewEntryForm />
+        </Container>
+    );
 }
 
 export default App;
---
+++ src/components/DisplayBalances.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Grid, Segment } from "semantic-ui-react";
+import DisplayBalance from "./DisplayBalance";
+
+function DisplayBalances({ title, type = "h1" }) {
+    return (
+        <Segment textAlign="center">
+        <Grid columns={2} defided>
+            <Grid.Row>
+                <Grid.Column>
+                    <DisplayBalance
+                        title="Income:"
+                        value="123.00"
+                        color="green"
+                    />
+                </Grid.Column>
+                <Grid.Column>
+                    <DisplayBalance
+                        title="Expenses:"
+                        value="456.00"
+                        color="red"
+                    />
+                </Grid.Column>
+            </Grid.Row>
+        </Grid>
+    </Segment>
+    );
+}
+
+export default DisplayBalances;
